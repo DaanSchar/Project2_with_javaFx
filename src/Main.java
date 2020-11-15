ba5df70cfeb6b3fc170bdc1434144b6d8058bffa
DaanSchar <Daan.Schaer@Gmail.com>
2020-11-15 22:18:09 +0100

    Commit testing test 2

--- src/Main.java
+++ src/Main.java
@@ -41,12 +41,6 @@ public class Main extends Application
         n = graph.getN();
         m = graph.getM();
 
-        //  test
-        for(int i = 0; i < 4; i++)
-        {
-            System.out.println("test");
-        }
-
         launch(args);
     }
 
