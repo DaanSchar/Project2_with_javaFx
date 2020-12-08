b0c663057d54fa21a7d6961da76bf949f8a4c070
leoniekerken <leo.niekerken@freenet.de>
2020-12-08 14:59:03 +0100

    -added: HintsUsed class to store which hints have been used -this is taken into account when calculating scores in Results class

---
+++ src/visuals/game/HintsUsed.java
@@ -0,0 +1,20 @@
+/**
+ * class that stores which Hints have been used
+ * @author Leo
+ */
+
+package visuals.game;
+
+public class HintsUsed {
+
+    public static boolean lineHintUsed;
+    public static boolean hoverHintUsed;
+    public static boolean smallestHintUsed;
+    public static boolean largestHintUsed;
+
+    public HintsUsed()
+    {
+
+    }
+
+}
